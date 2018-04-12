bfe87f240691860e64ad934017b0c91c169a60ff
Devanand <devanand@Devanands-MacBook-Pro-2.local>
2018-04-12 16:42:34 +0530

    updated fb link

--- components/GlobalHeader.vue
+++ components/GlobalHeader.vue
@@ -22,7 +22,7 @@
 					<div class="navbar-item">
 						<div class="field is-grouped">
 							<p class="control">
-								<a class="bd-tw-button button" data-social-network="Twitter" data-social-action="tweet" data-social-target="http://localhost:4000" target="_blank" href="">
+								<a class="bd-tw-button button" data-social-network="Facebook" data-social-action="tweet" data-social-target="https://www.facebook.com/Samikshafashionjewellery" target="_blank" href="https://www.facebook.com/Samikshafashionjewellery">
 									<span class="icon">
 										<i class="fa fa-facebook"></i>
 									</span>
